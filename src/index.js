a65458ddc4c6bb5f8c5a8cf500ca6a9073721846
digitaleAffen <digitaleaffen@gmail.com>
2023-02-03 21:46:45 +0100

    Added the functionality of the app and the City selector

--- src/index.js
+++ src/index.js
@@ -10,3 +10,25 @@ setInterval(function () {
     .tz("America/Los_Angeles")
     .format("HH:mm:ss")}`;
 });
+
+// connect the selector with the info showed bellow
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let: cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city-container" id="los-angeles">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime
+            .tz("America/Los_Angeles")
+            .format("HH:mm:ss")}</div>
+        </div>
+  `;
+}
+
+let citySelectorElement = document.querySelector("#city-selector");
+citySelectorElement.addEventListener("change", updateCity);

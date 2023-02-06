d677e19189e2edcd983caa776704d08bb7818480
digitaleAffen <digitaleaffen@gmail.com>
2023-02-06 15:39:58 +0100

    I added the information of the current location, so that it compares with the city that you searched for

--- src/index.js
+++ src/index.js
@@ -20,6 +20,39 @@ setInterval(function () {
   losAngelesTimeElement.innerHTML = `${losAngelesTime
     .tz("America/Los_Angeles")
     .format("HH:mm:ss")}`;
+
+  //Madrid
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  let madridTime = moment();
+
+  madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = `${madridTime
+    .tz("Europe/Madrid")
+    .format("HH:mm:ss")}`;
+
+  //Melbourne
+  let melbourneElement = document.querySelector("#melbourne");
+  let melbourneDateElement = melbourneElement.querySelector(".date");
+  let melbourneTimeElement = melbourneElement.querySelector(".time");
+  let melbourneTime = moment();
+
+  melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+  melbourneTimeElement.innerHTML = `${melbourneTime
+    .tz("Australia/Melbourne")
+    .format("HH:mm:ss")}`;
+
+  //Melbourne
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment();
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime
+    .tz("Asia/Tokyo")
+    .format("HH:mm:ss")}`;
 });
 
 // connect the selector with the info showed bellow
@@ -32,7 +65,19 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
+
+  let currentCityTimeZone = moment.tz.guess();
+  let currentCityName = currentCityTimeZone.replace("_", " ").split("/")[1];
+  let currentCityTime = moment().tz(currentCityTimeZone);
+  console.log(cityTime);
   citiesElement.innerHTML = `
+  <div class="city-container">
+          <div>
+          <h2>📍${currentCityName}</h2>
+          <div class="date">${currentCityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${currentCityTime.format("HH:mm:ss")}</div>
+        </div>
   <div class="city-container">
           <div>
             <h2>${cityName}</h2>
